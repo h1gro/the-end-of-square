b52ca9a228ea2411d5b95bd0c146485df23a678a
h1gro <komarov.aa@phystech.edu>
2024-08-27 19:30:54 +0300

    THE END SQUARE

--- Untitled4.cpp
+++ Untitled4.cpp
@@ -2,200 +2,291 @@
 #include <stdio.h>
 #include <math.h>
 
-struct Str_for_tester
+/**
+ * @brief Tests Structure for tests
+ * @param a square equation coefficient
+ * @param b square equation coefficient
+ * @param с square equation coefficient
+ * @param x1exp the first expected root
+ * @param x2exp the second expected root
+ * @param nRootsExp the expected number of roots
+ */
+struct Tests
 {
     double a, b, c;
     double x1exp, x2exp;
     int nRootsExp;
 };
 
-int SolveSquare ( double a, double b, double c, double* x1, double* x2);
-void clrbuf();
-int Tests_Check();
-bool is_equal(double elem1, double elem2);
-int tester(struct Str_for_tester str_test, int nTest);
+/**
+ * @brief Find roots of square equation with
+ * coefficients a, b, c and the number of roots
+ * @return Return the number of roots
+ * @param a square equation coefficient
+ * @param b square equation coefficient
+ * @param с square equation coefficient
+ * @param x1 the first root
+ * @param x2 the second root
+ */
+int SolveSquare(double a, double b, double c, double* x1, double* x2);
 
-const int NO_ROOTS = 0;
-const int ONE_ROOT = 1;
-const int TWO_ROOTS = 2;
-const int INFINITY_ROOTS = 3;
-const int TEST_DONE = 4;
-const int TEST_FAILED = 5;
-const int ALL_TESTS_DONE = 6;
-const int INCORRECT_TEST = 7;
-const double EPSILON = 0.0001;
+/**
+ * @brief Open the test from file and scan tests from it
+ * @return Return results of all tests
+ */
+int ScanTestsFile();
 
-int SolveSquare ( double a, double b, double c, double* x1, double* x2)
+/**
+ * @brief Compare roots from functions SolveSquare and SolveLinear with expected roots
+ * @return Return the result of the test
+ * @param nTest the number of the test
+ * @param struct_test structure where keeps coefficients
+ * a, b, c, roots x1exp, x2exp and nRootsExp
+ */
+int Tester(struct Tests struct_test, int nTest);
+
+/**
+ * @brief Find roots of linear equation with
+ * coefficients b, c and number of roots
+ * @return Return the number of roots
+ * @param b linear equation coefficient
+ * @param с linear equation coefficient
+ * @param x1 first root
+ */
+int SolveLinear(double b, double c, double* x1);
+
+/**
+ * @brief Print the result of the programm
+ * @param nRoots the number of roots
+ * @param x1 first root
+ * @param x2 second root
+ */
+void PrintResult(int nRoots, double x1, double x2);
+
+/**
+ * @brief Clear buffer from wrong coefficients
+ */
+void ClearBuffer();
+
+/**
+ * @brief Compare abs difference between two elements and epsilon
+ * @return Return True or False
+ * @param elem1 the first element
+ * @param elem2 the second element
+ */
+bool IsEqual(double elem1, double elem2);
+
+/**
+ * @brief Number_of_roots enumeration of constants that mean number of roots
+ * @param NO_ROOTS the number of roots is zero
+ * @param ONE_ROOT the number of roots is one
+ * @param TWO_ROOTS the number of roots is two
+ * @param INFINITY_ROOTS the number of roots is infinity
+ */
+enum Number_of_roots
 {
-        assert(x1);
-        assert(x2);
-        double D = 0;
-        D = b * b - 4 * a * c;
-        if ( is_equal(a, 0) )
-        {
-            if ( is_equal(b, 0) )
-            {
-                if ( is_equal(c, 0) )
-                {
-                    return INFINITY_ROOTS;
-                }
-                else
-                {
-                    return NO_ROOTS;
-                }
-            }
-            else
-            {
-                *x1 = -c / b;
-                return ONE_ROOT;
-            }
-        }
-        else
-        {
-            if (D >= 0)
-            {
-                *x1 = (-b + sqrt(D)) / (2 * a);
-                *x2 = (-b - sqrt(D)) / (2 * a);
-                if ( is_equal(*x1, *x2) )
-                {
-                    return ONE_ROOT;
-                }
-                else
-                {
-                    return TWO_ROOTS;
-                }
+    NO_ROOTS = 0,
+    ONE_ROOT = 1,
+    TWO_ROOTS = 2,
+    INFINITY_ROOTS = 3,
+};
 
-            }
-            else
-            {
-                return NO_ROOTS;
-            }
-        }
-}
+/**
+ * @brief Tests_results enumeration of constants that mean results of tests
+ * @param TEST_DONE the test has gone successfully
+ * @param TEST_FAILED the test has gone unsuccessfully
+ * @param ALL_TESTS_DONE all test have gone successfully
+ * @param INCORRECT_TEST the test is not correct
+ * @param FILE_NOT_OPENED the file is not opened
+ */
+enum Tests_results
+{
+    TEST_DONE = 4,
+    TEST_FAILED = 5,
+    ALL_TESTS_DONE = 6,
+    INCORRECT_TEST = 7,
+    FILE_NOT_OPENED = 8,
+};
+/**
+ * @param EPSILON the minimum difference between two
+ * elements in which that two elements can be equal
+ */
+const double EPSILON = 0.0001;
 
 int main()
 {
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
 
-    int identificator = Tests_Check();
+    int identificator = ScanTestsFile();
 
     if (identificator == ALL_TESTS_DONE)
     {
         while (scanf("%lg %lg %lg", &a, &b, &c) != 3)
         {
             printf("wrong input");
-            clrbuf();
+            ClearBuffer();
         }
+
         int nRoots = SolveSquare (a, b, c, &x1, &x2);
-
-        //txSetConsoleAttr (0x4E);
-        switch(nRoots)
-        {
-            case NO_ROOTS:       printf("no roots\n");
-                                 break;
-
-            case ONE_ROOT:       printf("%lg\n", x1);
-                                 break;
-
-            case TWO_ROOTS:      printf("%lg %lg\n", x1, x2);
-                                 break;
-
-            case INFINITY_ROOTS: printf("any number");
-                                 break;
-
-            default:             printf("unreachable");
-                                 break;
-
-        }
-        //txSetConsoleAttr (0x07);
+        PrintResult(nRoots, x1, x2);
     }
-
 }
 
-void clrbuf()
-{
 
-    //printf("вошёл в функцию");
+int SolveSquare ( double a, double b, double c, double* x1, double* x2)
+{
+    assert(x1);
+    assert(x2);
+
+    double D = b * b - 4 * a * c;
+
+    if ( IsEqual(a, 0) )
+    {
+        return SolveLinear(b, c, x1);
+    }
+    else
+    {
+        if (D >= 0)
+        {
+            double sqrt_D = sqrt(D);
+            *x1 = (-b + sqrt_D) / (2 * a);
+            *x2 = (-b - sqrt_D) / (2 * a);
+            if ( IsEqual(*x1, *x2) )
+            {
+                return ONE_ROOT;
+            }
+            else
+            {
+                return TWO_ROOTS;
+            }
+        }
+        else
+        {
+            return NO_ROOTS;
+        }
+    }
+}
+
+int SolveLinear(double b, double c, double* x1)
+{
+        assert(x1);
+
+        if ( IsEqual(b, 0) )
+        {
+            if ( IsEqual(c, 0) )
+            {
+                return INFINITY_ROOTS;
+            }
+            else
+            {
+                return NO_ROOTS;
+            }
+        }
+        else
+        {
+            *x1 = -c / b;
+            return ONE_ROOT;
+        }
+}
+
+void ClearBuffer()
+{
     int ch = getchar();
     while (ch != EOF && ch != '\n' )
     {
-        //printf("я выкинул символ, (%c), %d\n", ch, ch);
         ch = getchar();
     }
-    //printf("вышел из функции");
 }
 
-int tester(struct Str_for_tester str_test, int nTest)
+int Tester(struct Tests struct_test, int nTest)
 {
-    //printf("я зашёл в функцию\n");
 
     double x1 = 0, x2 = 0;
 
-    printf("%d: a = %lg, b = %lg, c = %lg\n", __LINE__, str_test.a, str_test.b, str_test.c);
+    int nRoots = SolveSquare(struct_test.a, struct_test.b, struct_test.c, &x1, &x2);
 
-    int nRoots = SolveSquare(str_test.a, str_test.b, str_test.c, &x1, &x2);
-
-    printf("%d: a = %lg, b = %lg, c = %lg\n", __LINE__, str_test.a, str_test.b, str_test.c);
-
-    if (nRoots != str_test.nRootsExp || !is_equal(x1, str_test.x1exp) || !is_equal(x2, str_test.x2exp))
+    if (nRoots != struct_test.nRootsExp
+        || !IsEqual(x1, struct_test.x1exp)
+        || !IsEqual(x2, struct_test.x2exp))
     {
-        printf("%d: a = %lg, b = %lg, c = %lg\n", __LINE__, str_test.a, str_test.b, str_test.c);
-
         printf("\n" "error test = %d, a = %lg, b = %lg, c = %lg\n"
                " x1 = %lg, x2 = %lg, nRoots = %d\n"
                "expected, x1exp = %lg, x2exp = %lg, nRootsExp = %d\n"
                "x1-x1exp = %lg, x2-x2exp= %lg\n\n",
-               nTest, str_test.a, str_test.b, str_test.c,
+               nTest, struct_test.a, struct_test.b, struct_test.c,
                x1, x2, nRoots,
-               str_test.x1exp, str_test.x2exp, str_test.nRootsExp,
-               x1-str_test.x1exp, x2-str_test.x2exp);
+               struct_test.x1exp, struct_test.x2exp, struct_test.nRootsExp,
+               x1-struct_test.x1exp, x2-struct_test.x2exp);
 
         return TEST_FAILED;
     }
     else
     {
-        //txSetConsoleAttr (0x4E);
-
         printf("Successfully\n");
-
-        //txSetConsoleAttr (0x07);
 
         return TEST_DONE;
     }
-
 }
 
-int Tests_Check()
+int ScanTestsFile()
 {
-    struct Str_for_tester tests[] = {{0, 1, -2, 2, 0, 1},
-                                     {0, 0, 0, 0, 0, 3},
-                                     {0, 1, -2, 2, 0, 1},
-                                     {0, 0, -1243, 0, 0, 0},
-                                     {0, 65, 0, 0, 0, 1},
-                                     {1, 2, -3, 1, -3, 2},
-                                     {1, 2, 3, 0, 0, 0},
-                                     {1.33, -2.6, -7.5, 3.54542, -1.59053, 2},
-                                     {0.09, 69.96, -1.87, 0.0267286, -777.36, 2},
-                                     {1, 2, 1, -1, -1, 1}};
+    struct Tests struct_test = {};
+    int nTest = 0;
 
-    //printf("%d %d\n", sizeof mass, sizeof mass[0]);
-    for (int i = 0; i < (sizeof tests)/(sizeof tests[0]); i++)
+    FILE * file = fopen("tests.txt", "r");
+    if (file == NULL)
+    {
+        printf("file not opened");
+        return FILE_NOT_OPENED;
+    }
+
+    while( fscanf(file,
+                  "%lg %lg %lg %lg %lg %d",
+                    &struct_test.a, &struct_test.b, &struct_test.c,
+                    &struct_test.x1exp, &struct_test.x2exp, &struct_test.nRootsExp) == 6)
     {
         int check = 0;
-        //printf("%d\n", i);
-        int nTest = i;
-        check = tester( tests[ i ], nTest);
-        if ( check == TEST_FAILED)
+
+        check = Tester(struct_test, nTest);
+
+        if (check == TEST_FAILED)
         {
+            fclose(file);
             return INCORRECT_TEST;
         }
+
+        nTest++;
     }
+    fclose(file);
     return ALL_TESTS_DONE;
 }
 
-bool is_equal(double elem1, double elem2)
+bool IsEqual(double elem1, double elem2)
 {
     return (fabs(elem1 - elem2) <= EPSILON);
+}
+
+void PrintResult(int nRoots, double x1, double x2)
+{
+    switch(nRoots)
+    {
+        case NO_ROOTS:       printf("no roots\n");
+                             break;
+
+        case ONE_ROOT:       printf("%lg\n", x1);
+                             break;
+
+        case TWO_ROOTS:      printf("%lg %lg\n", x1, x2);
+                             break;
+
+        case INFINITY_ROOTS: printf("any number");
+                             break;
+
+        default:             printf("unreachable");
+                             break;
+
+    }
+
 }
 
 
